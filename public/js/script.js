65767e1edb7dcce69f4e9c720efb183c83471762
Egor Arbuzov <egorarbuzov@gmail.com>
2025-02-23 08:45:52 +0300

    scrollbar

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const container = document.querySelector('.image-scroll-container');
-
-container.addEventListener('wheel', (event) => {
-    event.preventDefault(); // Предотвращаем стандартное поведение прокрутки
-    container.scrollLeft += event.deltaY; // Прокручиваем контейнер влево или вправо
-});
